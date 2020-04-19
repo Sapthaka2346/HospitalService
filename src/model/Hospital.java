a4587eb679054532e8d5239eb8a5abc334e5194f
Sapthaka Godage <53206450+Sapthaka2346@users.noreply.github.com>
2020-04-19 12:36:45 -0700

    HospitalAppointments

--- src/model/Hospital.java
+++ src/model/Hospital.java
@@ -162,10 +162,7 @@ public class Hospital {
 				return "Error while connecting to the database for reading.";
 			}
 			// Prepare the html table to be displayed
-	        output = "<table border=\"1\">"
-	        + "<tr><th>Appointment Type</th><th>Appointment Number</th><th>Appointment   "
-	   		+ "Description</th><th>Appointment          "
-	   		+ "Date</th><th>User ID</th><th>Doctor ID</th><th>Hospital ID</th><th>Update</th><th>Remove</th></tr>"; 
+	        output = "<table border=\"1\"><tr><th>Appointment ID</th><th>Appointment type</th><th>Appointment no</th><th>Appointment Date</th><th><Appointment Description</th><th>User ID</th><th>Doctor ID</th><th>Hospital ID</th><th>Update</th><th>Remove</th></tr>"; 
 	 
 	        String query = "select * from appointment where Hospital_ID="+ Hospital_ID1;    
 	        Statement stmt = con.createStatement();   
@@ -174,21 +171,22 @@ public class Hospital {
 	  // iterate through the rows in the result set  
 	       while (rs.next())    
 	       {
-		       String appointment_ID = Integer.toString(rs.getInt("appointment_ID"));  
-		       String appointment_type = rs.getString("appointment_type");   
-		       String appointment_no = rs.getString("appointment_no");     
-		       String appointment_desc = rs.getString(("appointment_desc"));  
-		       String appointment_date = rs.getString("appointment_date");
+		       String Appointment_ID = Integer.toString(rs.getInt("Appointment_ID"));  
+		       String Appointment_type = rs.getString("Appointment_type");   
+		       String Appointment_no = rs.getString("Appointment_no");      
+		       String Appointment_date = rs.getString("Appointment_date");
+		       String Appointment_description = rs.getString(("Appointment_description")); 
 		       String Ruser_ID = rs.getString("Ruser_ID");
 		       String Rdoctor_ID = rs.getString("Rdoctor_ID");
 		       String Hospital_ID = rs.getString("Hospital_ID");
 	   
 	   
 	    // Add into the html table  
-		       output += "<tr><td>" + appointment_type + "</td>";     
-		       output += "<td>" + appointment_no + "</td>";     
-		       output += "<td>" + appointment_desc + "</td>";    
-		       output += "<td>" + appointment_date + "</td>"; 
+		       output += "<tr><td>" + Appointment_ID + "</td>";     
+		       output += "<td>" + Appointment_type + "</td>";  
+		       output += "<td>" + Appointment_no + "</td>";        
+		       output += "<td>" + Appointment_date + "</td>"; 
+		       output += "<td>" + Appointment_description + "</td>";
 		       output += "<td>" + Ruser_ID + "</td>";     
 		       output += "<td>" + Rdoctor_ID + "</td>";    
 		       output += "<td>" + Hospital_ID + "</td>";
@@ -199,7 +197,7 @@ public class Hospital {
 				   + "<td><form method=\"post\" action=\"appointments.jsp\">"      
 		   		+ "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\"     "
 		   		+ " class=\"btn btn-danger\">"     
-		   		+ "<input name=\"appointment_ID\" type=\"hidden\" value=\"" + appointment_ID     
+		   		+ "<input name=\"appointment_ID\" type=\"hidden\" value=\"" + Appointment_ID     
 		   		+ "\">" + "</form></td></tr>";    
 		    }
 	       
@@ -208,7 +206,7 @@ public class Hospital {
 	   // Complete the html table    
 	         output += "</table>";  
 		} catch (Exception e) {
-			output = "Error while reading the users appointments.";
+			output = "Error while reading the hospitals appointments.";
 			System.err.println(e.getMessage());
 		}
 		return output;
